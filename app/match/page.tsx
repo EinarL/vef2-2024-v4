5d3dc15702ff9a4ccfc382f90dadfa460c0d5d1d
Einar Logi Pétursson <einarlogip@outlook.com>
2024-03-20 22:24:54 +0000

    match síða

--- app/match/page.tsx
+++ app/match/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 import { Navbar } from "@/components/Navbar";
-import { Match } from "../matches/page";
-import { useParams, useSearchParams  } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useSearchParams  } from 'next/navigation';
+import styles from "../../public/styles/match.module.css";
+
 
 export interface MatchInfo {
     id: number;
@@ -24,7 +23,8 @@ export default function Home() {
     let matchInfo = null;
     if(matchInfoStringified) matchInfo = JSON.parse(matchInfoStringified);
     console.log(matchInfo);
-
+    let winner = matchInfo.home_score > matchInfo.away_score ? matchInfo.homeTeam : matchInfo.awayTeam;
+    if(matchInfo.home_score === matchInfo.away_score) winner = null;
 
     return (
         <div>
@@ -32,7 +32,26 @@ export default function Home() {
         <main>
             <Navbar/>
             {matchInfo ? (
-                <h4>{matchInfo.id}</h4>
+                <table className={styles.match}>
+                    <thead>
+                        <tr>
+                            <th colSpan={3}>{formatDate(matchInfo.date)}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{matchInfo.homeTeam}</td>
+                            <td>VS</td>
+                            <td>{matchInfo.awayTeam}</td>
+                        </tr>
+                        <tr>
+                            <td>{matchInfo.home_score}</td>
+                            <td>Staða</td>
+                            <td>{matchInfo.away_score}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             ) : (
                 <h2>Error getting match information :(</h2>
             )}
@@ -41,3 +60,33 @@ export default function Home() {
     );
 
   };
+
+
+
+  /**
+ * @param {string} dateString       24. des 2024 kl. 15:20
+ * @returns {string} returns the date as a string in the form of: dd. month yyyy kl. hh:mm
+ */
+function formatDate(dateString: string){
+    const months: { [key: number]: string } = {
+        1: 'jan',
+        2: 'feb',
+        3: 'mars',
+        4: 'apríl',
+        5: 'maí',
+        6: 'júní',
+        7: 'júlí',
+        8: 'ágúst',
+        9: 'sept',
+        10: 'okt',
+        11: 'nóv',
+        12: 'des',
+    }
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Months are zero-based
+    const day = String(date.getDate());
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${day}. ${months[month]} ${year} kl. ${hours}:${minutes}`
+  }
